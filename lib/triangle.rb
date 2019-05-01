58cc905771847ab50416a0043a31f9960e2b32cc
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-05-01 23:50:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,13 @@
 class Triangle
   # write code here
   attr_accessor :equilateral, :isosceles, :scalene
-  def initialize(equilateral, isosceles, scalene)
+  def initialize(side1,side2,side3)
   end
+
+  def kind()
+  end
+
+end
+
+class TriangleError < StandardError
 end
