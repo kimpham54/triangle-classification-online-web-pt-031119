b2abd3c7622bd2899ef476b41a993b688dddf509
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-05-02 00:36:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,25 +10,17 @@ class Triangle
     #    end
     #  end
     if ((side1 <= 0) | (side2 <= 0) | (side3 <= 0))
-      # begin
        raise TriangleError
-    #  rescue TriangleError => error
-        #  puts error.message
-      #  end
-    elsif ((side1 == side2 && side2 == side3) && (side1 > 0) && (side2 > 0) && (side3 > 0))
+     elsif ((side1+side2 < side3) | (side2+side3 < side1) | (side1+side3 < side2))
+       raise TriangleError
+     elsif ((side1 == side2 && side2 == side3)
       @kind = :equilateral
-    elsif ((side1+side2 > side3) | (side2+side3 > side1) | (side1+side3 > side2) && (side1 == side2) | (side2 == side3) | (side1 == side3) && ((side1 > 0) && (side2 > 0) && (side3 > 0)))
+    elsif ((side1 == side2) | (side2 == side3) | (side1 == side3))
       @kind = :isosceles
-    elsif ((side1+side2 > side3) | (side2+side3 > side1) | (side1+side3 > side2) && ((side1 > 0) && (side2 > 0) && (side3 > 0)))
+    elsif ((side1+side2 > side3) | (side2+side3 > side1) | (side1+side3 > side2))
       @kind = :scalene
-elsif ((side1+side2 < side3) | (side2+side3 < side1) | (side1+side3 < side2))
-  raise TriangleError
-     else
-      # begin
-       raise TriangleError
-    #  rescue TriangleError => error
-        #  puts error.message
-      #  end
+    else
+      raise TriangleError
   end
 end
 
