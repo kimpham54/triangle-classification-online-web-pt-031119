e061822052400fec6222cbc6434632f5ca054492
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-05-02 00:25:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
       @kind = :isosceles
     elsif ((side1+side2 > side3) | (side2+side3 > side1) | (side1+side3 > side2) && ((side1 > 0) && (side2 > 0) && (side3 > 0)))
       @kind = :scalene
-    elsif ((side1 > 0) && (side2 > 0) && (side3 > 0)) == false
+    elsif ((side1 > 0) | (side2 > 0) | (side3 > 0))
       begin
        raise TriangleError
      rescue TriangleError => error
