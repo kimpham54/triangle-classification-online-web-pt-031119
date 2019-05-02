c53ce5bfe97042b71982246b6579468ee4dc0786
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-05-02 00:02:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,42 @@
 class Triangle
   # write code here
-  attr_accessor :equilateral, :isosceles, :scalene
+  attr_accessor :kind
   def initialize(side1,side2,side3)
+    if side1 == 0 | side2 == 0 | side3 == 0
+      begin
+       raise TriangleError
+     rescue TriangleError => error
+         puts error.message
+     end
+
+
+    if side1 == side2 == side3 && side1 > 0
+      @kind = :equilateral
+    elsif
+      side1+side2 > side3 | side2+side3 > side1 | side1+side3 > side2 && side1 == side2 | side2 == side3 | side1 == side3
+      @kind = :isosceles
+    elsif
+      side1+side2 > side3 | side2+side3 > side1 | side1+side3 > side2
+      @kind = :scalene
+    else
+      begin
+       raise TriangleError
+     rescue TriangleError => error
+         puts error.message
   end
 
   def kind()
+    @kind
   end
 
-end
+
 
 class TriangleError < StandardError
+  def message
+  "you must give the get_married method an argument of an instance of the person class!"
+end
+end
+
+
+
 end
