5f4ac679f5e53e560404e05bb1d728cfc5acb9c0
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-05-02 00:14:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
       @kind = :equilateral
     elsif ((side1+side2 > side3) | (side2+side3 > side1) | (side1+side3 > side2) && (side1 == side2) | (side2 == side3) | (side1 == side3) && (side1 > 0 | side2 > 0 | side3 > 0))
       @kind = :isosceles
-    elsif ((side1+side2 > side3) | (side2+side3 > side1) | (side1+side3 > side2)  && (side1 > 0 | side2 > 0 | side3 > 0))
+    elsif ((side1+side2 > side3) | (side2+side3 > side1) | (side1+side3 > side2) && (side1 > 0 | side2 > 0 | side3 > 0))
       @kind = :scalene
     else
       begin
