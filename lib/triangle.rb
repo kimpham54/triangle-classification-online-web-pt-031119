3eacaa1c2fdbe0e204bf7bcb6e874489be70b20f
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-05-02 00:06:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,14 +2,12 @@ class Triangle
   # write code here
   attr_accessor :kind
   def initialize(side1,side2,side3)
-    if side1 == 0 | side2 == 0 | side3 == 0
+    if ((side1 == 0) | (side2 == 0) | (side3 == 0))
       begin
        raise TriangleError
      rescue TriangleError => error
          puts error.message
      end
-
-
     if side1 == side2 == side3 && side1 > 0
       @kind = :equilateral
     elsif
