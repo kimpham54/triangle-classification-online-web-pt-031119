2ddc74216a9f42b5e117fc6c6b686a636d9c4356
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-05-02 00:06:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
      rescue TriangleError => error
          puts error.message
      end
-    if side1 == side2 == side3 && side1 > 0
+    if (side1 == side2 && side2 == side3 && side1 > 0)
       @kind = :equilateral
     elsif
       side1+side2 > side3 | side2+side3 > side1 | side1+side3 > side2 && side1 == side2 | side2 == side3 | side1 == side3
