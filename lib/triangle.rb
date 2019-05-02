1a386941cfc322f63033d15266573123a165a785
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-05-02 00:14:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,18 +2,18 @@ class Triangle
   # write code here
   attr_accessor :kind
   def initialize(side1,side2,side3)
-    if ((side1 == 0) | (side2 == 0) | (side3 == 0))
-      begin
-       raise TriangleError
-     rescue TriangleError => error
-         puts error.message
-       end
-     end
-    if (side1 == side2 && side2 == side3 && side1 > 0)
+    # if ((side1 == 0) | (side2 == 0) | (side3 == 0))
+    #   begin
+    #    raise TriangleError
+    #  rescue TriangleError => error
+    #      puts error.message
+    #    end
+    #  end
+    if ((side1 == side2 && side2 == side3 && side1 > 0) && (side1 > 0 | side2 > 0 | side3 > 0))
       @kind = :equilateral
-    elsif ((side1+side2 > side3) | (side2+side3 > side1) | (side1+side3 > side2) && (side1 == side2) | (side2 == side3) | (side1 == side3))
+    elsif ((side1+side2 > side3) | (side2+side3 > side1) | (side1+side3 > side2) && (side1 == side2) | (side2 == side3) | (side1 == side3) && (side1 > 0 | side2 > 0 | side3 > 0))
       @kind = :isosceles
-    elsif ((side1+side2 > side3) | (side2+side3 > side1) | (side1+side3 > side2))
+    elsif ((side1+side2 > side3) | (side2+side3 > side1) | (side1+side3 > side2)  && (side1 > 0 | side2 > 0 | side3 > 0))
       @kind = :scalene
     else
       begin
